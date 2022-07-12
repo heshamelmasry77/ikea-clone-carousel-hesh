033affe1c6babacece8ca7339d931422fae67865
Hesham <heshamelmasry1992@gmail.com>
2022-07-12 20:40:32 +0200

    use block scope to avoid conflicts with other project variables

--- script.js
+++ script.js
@@ -1,91 +1,92 @@
 window.addEventListener('load', (event) => {
+    {
+        // Get elements
+        const carouselWrapper = document.querySelector(".carousel-wrapper");
+        const carousel = document.querySelector(".carousel");
+        const carouselItem = document.querySelector(".carousel__item");
+        const carouselOffsetWidth = carousel.offsetWidth;
+        const carouselScrollWidth = carousel.scrollWidth;
+        const carouselItemOffsetWidth = carouselItem.offsetWidth;
+        const nextBtn = document.querySelector("#carousel__button--next");
+        const prevBtn = document.querySelector("#carousel__button--prev");
 
-    // Get elements
-    const carouselWrapper = document.querySelector(".carousel-wrapper");
-    const carousel = document.querySelector(".carousel");
-    const carouselItem = document.querySelector(".carousel__item");
-    const carouselOffsetWidth = carousel.offsetWidth;
-    const carouselScrollWidth = carousel.scrollWidth;
-    const carouselItemOffsetWidth = carouselItem.offsetWidth;
-    const nextBtn = document.querySelector("#carousel__button--next");
-    const prevBtn = document.querySelector("#carousel__button--prev");
+        let carouselScrollLeftWidth = carousel.scrollLeft;
 
-    let carouselScrollLeftWidth = carousel.scrollLeft;
+        //Handle Next BTN click
+        nextBtn.addEventListener('click', function () {
+            carouselScrollLeftWidth += carouselItemOffsetWidth;
+            if ((carouselScrollLeftWidth + carouselOffsetWidth) >= carouselScrollWidth) {
+                hideNextBTN();
+                showPrevBTN();
+            } else {
+                showPrevBTN();
+            }
+            carousel.scrollLeft = carouselScrollLeftWidth;
+        });
 
-    //Handle Next BTN click
-    nextBtn.addEventListener('click', function () {
-        carouselScrollLeftWidth += carouselItemOffsetWidth;
-        if ((carouselScrollLeftWidth + carouselOffsetWidth) >= carouselScrollWidth) {
+        //Handle Prev BTN click
+        prevBtn.addEventListener('click', () => {
+            carouselScrollLeftWidth -= carouselItemOffsetWidth;
+            if (carouselScrollLeftWidth <= 0) {
+                showNextBTN();
+                hidePrevBTN();
+                carouselScrollLeftWidth = 0;
+            } else {
+                showNextBTN();
+            }
+            carousel.scrollLeft = carouselScrollLeftWidth;
+        });
+
+
+        // Show Carousel arrows on carousel hover
+        carouselWrapper.addEventListener("mouseover", showCarouselBTNs);
+
+        function showCarouselBTNs() {
+            // If I can slide to the right then hide the prev BTN
+            showNextBTN();
+            showPrevBTN();
+            // show the right arrow if we can scroll to the left. if I am at the end of the scroll left
+            if ((carouselScrollLeftWidth + carouselOffsetWidth) >= carouselScrollWidth) {
+                // The end of the scroll
+                // Hide right arrow
+                hideNextBTN();
+                showPrevBTN();
+            } else {
+                showPrevBTN();
+            }
+
+            // show the left arrow if I am at the right scroll
+            if (carouselScrollLeftWidth <= 0) {
+                showNextBTN();
+                hidePrevBTN();
+                carouselScrollLeftWidth = 0;
+            } else {
+                showPrevBTN();
+            }
+        }
+
+        // Hide Carousel arrows on carousel hover
+        carouselWrapper.addEventListener("mouseout", hideCarouselBTNs);
+
+        function hideCarouselBTNs() {
             hideNextBTN();
-            showPrevBTN();
-        } else {
-            showPrevBTN();
-        }
-        carousel.scrollLeft = carouselScrollLeftWidth;
-    });
-
-    //Handle Prev BTN click
-    prevBtn.addEventListener('click', () => {
-        carouselScrollLeftWidth -= carouselItemOffsetWidth;
-        if (carouselScrollLeftWidth <= 0) {
-            showNextBTN();
             hidePrevBTN();
-            carouselScrollLeftWidth = 0;
-        } else {
-            showNextBTN();
-        }
-        carousel.scrollLeft = carouselScrollLeftWidth;
-    });
-
-
-    // Show Carousel arrows on carousel hover
-    carouselWrapper.addEventListener("mouseover", showCarouselBTNs);
-
-    function showCarouselBTNs() {
-        // If I can slide to the right then hide the prev BTN
-        showNextBTN();
-        showPrevBTN();
-        // show the right arrow if we can scroll to the left. if I am at the end of the scroll left
-        if ((carouselScrollLeftWidth + carouselOffsetWidth) >= carouselScrollWidth) {
-            // The end of the scroll
-            // Hide right arrow
-            hideNextBTN();
-            showPrevBTN();
-        } else {
-            showPrevBTN();
         }
 
-        // show the left arrow if I am at the right scroll
-        if (carouselScrollLeftWidth <= 0) {
-            showNextBTN();
-            hidePrevBTN();
-            carouselScrollLeftWidth = 0;
-        } else {
-            showPrevBTN();
+        function showNextBTN() {
+            nextBtn.style.display = "block";
         }
-    }
 
-    // Hide Carousel arrows on carousel hover
-    carouselWrapper.addEventListener("mouseout", hideCarouselBTNs);
+        function showPrevBTN() {
+            prevBtn.style.display = "block";
+        }
 
-    function hideCarouselBTNs() {
-        hideNextBTN();
-        hidePrevBTN();
-    }
+        function hideNextBTN() {
+            nextBtn.style.display = "none";
+        }
 
-    function showNextBTN() {
-        nextBtn.style.display = "block";
-    }
-
-    function showPrevBTN() {
-        prevBtn.style.display = "block";
-    }
-
-    function hideNextBTN() {
-        nextBtn.style.display = "none";
-    }
-
-    function hidePrevBTN() {
-        prevBtn.style.display = "none";
+        function hidePrevBTN() {
+            prevBtn.style.display = "none";
+        }
     }
 });
